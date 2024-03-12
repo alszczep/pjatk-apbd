736619b607c88c1cc231de37677d8e79009f1d5c
alszczep <al.szczepura@gmail.com>
2024-03-12 19:27:34 +0100

    Add a method for calculating average of numbers

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+namespace Lab1;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(CalculateAverage(
+            [1, 2, 3, 4, 5]
+        ));
+    }
+
+    private static float CalculateAverage(int[] numbers)
+    {
+        float sum = 0;
+        foreach (var number in numbers) sum += number;
+        return sum / numbers.Length;
+    }
+}
