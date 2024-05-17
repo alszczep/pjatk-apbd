ef142afeeddc59ceb42e00d3d44741520c88b58a
alszczep <al.szczepura@gmail.com>
2024-05-17 15:20:59 +0200

    Generate models and context from db

--- Lab5/Api/Models/Client_Trip.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Api.Models;
-
-public partial class Client_Trip
-{
-    public int IdClient { get; set; }
-    public int IdTrip { get; set; }
-}
--- Lab5/Api/Models/Country.cs
+++ Lab5/Api/Models/Country.cs
@@ -1,8 +1,13 @@
-﻿namespace Api.Models;
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api.Models;
 
 public partial class Country
 {
     public int IdCountry { get; set; }
+
     public string Name { get; set; } = null!;
-    public virtual Trip Trip { get; set; } = null!;
+
+    public virtual ICollection<Trip> IdTrips { get; set; } = new List<Trip>();
 }
