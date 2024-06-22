06f5f763ac9c45f8fd2766891da64a805844c737
alszczep <al.szczepura@gmail.com>
2024-06-23 01:07:39 +0200

    Add client service tests

--- Project/Tests/ClientServiceTests/AddClientAsync.cs
+++ Project/Tests/ClientServiceTests/AddClientAsync.cs
@@ -1,23 +1,26 @@
-
+using Api.Services;
 using Shouldly;
+using Tests.Fakes;
 using Xunit.Abstractions;
-
 
 namespace Tests;
 
-public class Tests
+public class ClientsServiceTests
 {
+    private readonly ClientsService clientsService;
     private readonly ITestOutputHelper testOutputHelper;
 
-    public Tests(ITestOutputHelper testOutputHelper)
+    public ClientsServiceTests(ITestOutputHelper testOutputHelper)
     {
         this.testOutputHelper = testOutputHelper;
+        this.clientsService = new ClientsService(new FakeClientsRepository());
     }
 
     private Task TODO()
     {
         throw new InvalidOperationException("TODO");
     }
+
     [Fact]
     public async void Should_ThrowException_WhenDateIsMoreThatDateDue()
     {
